5d82351795c71311a1cd0905800c46c5ccff6463
권태웅 <kmeoung@gmail.com>
2019-12-16 00:55:19 +0900

    서버 작업 진행중

--- app/src/main/java/com/kmeoung/mapstore/ActivityLogin.kt
+++ app/src/main/java/com/kmeoung/mapstore/ActivityLogin.kt
@@ -3,14 +3,27 @@ package com.kmeoung.mapstore
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import com.google.gson.Gson
 import com.kmeoung.mapstore.base.BaseActivity
+import com.truevalue.dreamappeal.http.BaseHttpCallback
+import com.truevalue.dreamappeal.http.BaseHttpParams
+import com.truevalue.dreamappeal.http.BaseOkhttpClient
+import com.truevalue.dreamappeal.http.HttpType
 import kotlinx.android.synthetic.main.activity_login.*
+import okhttp3.Call
+import org.json.JSONObject
 
 class ActivityLogin : BaseActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val id = "nzwfbx"
+        val pw = "zx78776230"
+        et_email.setText(id)
+        et_password.setText(pw)
 
         onClickView()
     }
@@ -19,21 +32,52 @@ class ActivityLogin : BaseActivity(){
         val listener = View.OnClickListener{
             when(it){
                 btn_login->{
-                    val intent = Intent(this@ActivityLogin,ActivityMain::class.java)
-                    startActivity(intent)
-                    finish()
+                    login()
+//                    val intent = Intent(this@ActivityLogin,ActivityMain::class.java)
+//                    startActivity(intent)
+//                    finish()
                 }
                 tv_register->{
                     val intent = Intent(this@ActivityLogin,ActivityRegister::class.java)
                     startActivity(intent)
                 }
-                tv_forgot_password->{
-                    // todo : 비밀번호 찾기 페이지 연동
-                }
             }
         }
         btn_login.setOnClickListener(listener)
         tv_register.setOnClickListener(listener)
-        tv_forgot_password.setOnClickListener(listener)
+    }
+
+    private fun login(){
+
+        val params = BaseHttpParams()
+        params.put("id",et_email.text.toString())
+        params.put("pw",et_password.text.toString())
+
+        BaseOkhttpClient.request(
+            HttpType.GET,
+            Comm_Params.URL_SELECT_MEMBER,
+            null,
+            params,
+            object : BaseHttpCallback{
+                override fun onResponse(
+                    call: Call,
+                    serverCode: Int,
+                    body: String,
+                    code: String,
+                    message: String
+                ) {
+                    val json = JSONObject(body)
+                    val error =  json.getString("error");
+                    if(error.isNullOrEmpty()) { // 데이터가 있음
+                        Toast.makeText(this@ActivityLogin, json.toString(), Toast.LENGTH_SHORT)
+                            .show()
+                    }else{
+                        Toast.makeText(this@ActivityLogin, "아이디, 비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+            }
+
+        )
     }
 }
